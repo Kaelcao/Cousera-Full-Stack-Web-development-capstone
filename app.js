c989b653574c26c1b910138aa4db257968411619
Cao Anh Quan <aquancva@gmail.com>
2016-05-28 12:44:05 +0200

    up

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/api', routesApi);

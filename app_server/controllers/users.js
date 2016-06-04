68cb74772808c6d5db61ee7431c280b0344e39c0
Cao Anh Quan <aquancva@gmail.com>
2016-06-04 12:33:26 +0200

    xong routing va design

---
+++ app_server/controllers/users.js
@@ -0,0 +1,6 @@
+/**
+ * Created by caoanhquan on 5/28/16.
+ */
+module.exports.index = function (req, res) {
+    res.render('index', {title: 'user'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var routes = express.Router();
 var ctrlArticles = require('../controllers/articles');
+var ctrlUsers = require('../controllers/users');
+
 
 routes.get('/', ctrlArticles.index);
+routes.get('/article', ctrlArticles.article);
+routes.get('/newarticle', ctrlArticles.newArticle);
+routes.get('/category', ctrlArticles.category);
+
+routes.get('/user', ctrlUsers.index);
 
 module.exports = routes;

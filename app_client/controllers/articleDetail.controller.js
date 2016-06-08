c21377d30bdb0cb80aa5f664d82a8c7e2eab19f6
Cao Anh Quan <aquancva@gmail.com>
2016-06-09 01:59:18 +0200

    up

--- app_client/controllers/articleDetail.controller.js
+++ app_client/controllers/articleDetail.controller.js
@@ -3,29 +3,30 @@
 	.module('yboxApp')
 	.controller('articleDetailCtrl', articleDetailCtrl);
 
-	articleDetailCtrl.$inject = ["$routeParams","articles"];
-	function articleDetailCtrl ($routeParams,articles) {
+	articleDetailCtrl.$inject = ["$routeParams","articles","categories"];
+	function articleDetailCtrl ($routeParams,articles,categories) {
 		var vm = this;
 		vm.articleid = $routeParams.articleid;
 
 		articles.getArticleById(vm.articleid)
 		.success(function(data){
 			vm.article = data;
-		})
-		.error(function(err){
-			console.log(e);
-		});
-
-		articles
-		.getAll(4,1)
-		.success(function(data){
-			vm.relatedArticles = data.filter(function(article){
-				return article._id != vm.articleid
+			categories
+			.articles(data.category_id,4,1)
+			.success(function(data){
+				vm.relatedArticles = data.filter(function(article){
+					return article._id != vm.articleid
+				});
+			})
+			.error(function(err){
+				console.log(e);
 			});
 		})
 		.error(function(err){
 			console.log(e);
 		});
+
+		
 
 	};
 })();

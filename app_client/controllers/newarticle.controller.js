83ec08c2e91e21e317dbc9feb5e430060eda0fc0
Cao Anh Quan <aquancva@gmail.com>
2016-06-09 02:11:31 +0200

    up

--- app_client/controllers/newarticle.controller.js
+++ app_client/controllers/newarticle.controller.js
@@ -3,39 +3,45 @@
 	.module("yboxApp")
 	.controller('newArticleCtrl',newArticleCtrl);
 
-	newArticleCtrl.$inject = ["categories","articles","$location"];
-	function newArticleCtrl(categories,articles,$location){
-		var vm = this;
-		categories.getAll()
-		.success(function(data){
-			vm.categories = data;
-		})
-		.error(function(err){
-			console.log(e);
-		});
-		vm.onSubmit = function () {
-			vm.formError = "";
-			if(!vm.formData || !vm.formData.title || !vm.formData.content || !vm.formData.category_id || !vm.formData.tags){
-				vm.formError = "All fields required, please try again";
-			} else {
-				
-				articles.addArticle({
-					title: vm.formData.title,
-					content: vm.formData.content,
-					category_id: vm.formData.category_id,
-					tags: vm.formData.tags,
-				})
-				.success(function(data){
-					console.log("success");
-					$location.path("/");
-				})
-				.error(function(data){
-					vm.formError = "Your article has not been saved,there is an error, try again";
-				});
-				console.log(vm.formData);
-				return false;	
+	newArticleCtrl.$inject = ["categories","articles","$location","authentication"];
+	function newArticleCtrl(categories,articles,$location,authentication){
+		if (authentication.isLoggedIn()){
+			var vm = this;
+			categories.getAll()
+			.success(function(data){
+				vm.categories = data;
+			})
+			.error(function(err){
+				console.log(e);
+			});
+			vm.onSubmit = function () {
+				vm.formError = "";
+				if(!vm.formData || !vm.formData.title || !vm.formData.content || !vm.formData.category_id || !vm.formData.tags){
+					vm.formError = "All fields required, please try again";
+				} else {
+
+					articles.addArticle({
+						title: vm.formData.title,
+						content: vm.formData.content,
+						category_id: vm.formData.category_id,
+						tags: vm.formData.tags,
+					})
+					.success(function(data){
+						console.log("success");
+						$location.path("/");
+					})
+					.error(function(data){
+						vm.formError = "Your article has not been saved,there is an error, try again";
+					});
+					console.log(vm.formData);
+					return false;	
+				}
+
 			}
-			
+		}else{
+			$location.path("/login");
+			console.log("Please sign in first");
 		}
+		
 	};
 })();
